a40a4b96427a7b15fc8fe1c7296b8926d82fa84e
Amro Ibrahim <amro.ibrahim@outlook.com>
2020-03-22 16:16:00 -0700

    Week014

---
+++ DIYDOOM/Week014/src/DIYDoom/Things.cpp
@@ -0,0 +1,29 @@
+#include "Things.h"
+
+Things::Things()
+{
+}
+
+Things::~Things()
+{
+}
+
+void Things::AddThing(Thing &thing)
+{
+    m_Things.push_back(thing);
+}
+
+Thing Things::GetThingByID(int iID)
+{
+    Thing thing;
+    for (std::vector<Thing>::iterator itr = m_Things.begin(); itr != m_Things.end(); ++itr)
+    {
+        if (itr->Type == iID)
+        {
+            thing = (*itr);
+            break;
+        }
+    }
+    return thing;
+}
+
